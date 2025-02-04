9b40da21035bbf408dd568a097d705a4ca3f3e4c
Antônio Evaldo <antonio.evaldo349@gmail.com>
2025-02-04 11:54:55 -0300

    Vídeo 5.5: Renderizando options com KeyValuePipe

--- src/app/form-nova-transacao/form-nova-transacao.component.ts
+++ src/app/form-nova-transacao/form-nova-transacao.component.ts
@@ -1,16 +1,19 @@
 import { Component, output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { TipoTransacao, Transacao } from '../modelos/transacao';
+import { KeyValuePipe } from '@angular/common';
 
 @Component({
   selector: 'app-form-nova-transacao',
-  imports: [FormsModule],
+  imports: [FormsModule, KeyValuePipe],
   templateUrl: './form-nova-transacao.component.html',
   styleUrl: './form-nova-transacao.component.css'
 })
 export class FormNovaTransacaoComponent {
   tipoTransacao = "";
   valorTransacao = "";
+
+  tipoTransacaoEnum = TipoTransacao;
 
   transacaoCriada = output<Transacao>();
 
